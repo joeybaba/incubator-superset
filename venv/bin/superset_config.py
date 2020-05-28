4d2fab9b2e242a9286e187af790d31ffc50518e9
joeybaba <scau2017@163.com>
2020-05-28 12:21:03 +0800

    init:add file and folder

---
+++ venv/bin/superset_config.py
@@ -0,0 +1,39 @@
+#---------------------------------------------------------
+# Superset specific config
+#---------------------------------------------------------
+import os
+ROW_LIMIT = 5000
+
+SUPERSET_WEBSERVER_PORT = 8000
+#---------------------------------------------------------
+
+#---------------------------------------------------------
+# Flask App Builder configuration
+#---------------------------------------------------------
+# Your App secret key
+SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
+
+# The SQLAlchemy connection string to your database backend
+# This connection defines the path to the database that stores your
+# superset metadata (slices, connections, tables, dashboards, ...).
+# Note that the connection information to connect to the datasources
+# you want to explore are managed directly in the web UI
+# add
+if "SUPERSET_HOME" in os.environ:
+    DATA_DIR = os.environ["SUPERSET_HOME"]
+else:
+    DATA_DIR = os.path.join(os.path.expanduser("~"), ".superset")
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(DATA_DIR, "superset.db")
+    # '/Users/joey/Documents/PycharmProjects/incubator-superset/.superset/superset.db'
+# 'sqlite:////path/to/superset.db'
+
+# Flask-WTF flag for CSRF
+WTF_CSRF_ENABLED = True
+# Add endpoints that need to be exempt from CSRF protection
+WTF_CSRF_EXEMPT_LIST = []
+# A CSRF token that expires in 1 year
+WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
+
+# Set this API key to enable Mapbox visualizations
+MAPBOX_API_KEY = ''
